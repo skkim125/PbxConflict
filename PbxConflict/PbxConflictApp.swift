dc426de2e353807992559e8eb0bc6eb622dd17fb
skkim125 <kthanks125@gmail.com>
2024-10-08 11:13:49 +0900

    Initial Commit

---
+++ PbxConflict/PbxConflictApp.swift
@@ -0,0 +1,17 @@
+//
+//  PbxConflictApp.swift
+//  PbxConflict
+//
+//  Created by 김상규 on 10/8/24.
+//
+
+import SwiftUI
+
+@main
+struct PbxConflictApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
